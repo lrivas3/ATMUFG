9f4e1d184a91c303d78858ea32fadab07691ab13
leonardo <84354110+lrivas3@users.noreply.github.com>
2023-12-01 14:41:57 -0600

    :sparkles: Add Database Script

---
+++ script_bd.sql
@@ -0,0 +1,170 @@
+CREATE DATABASE ATM
+go
+
+USE ATM
+go
+
+create table AspNetRoles
+(
+    Id               nvarchar(450) not null
+        constraint PK_AspNetRoles
+            primary key,
+    Name             nvarchar(256),
+    NormalizedName   nvarchar(256),
+    ConcurrencyStamp nvarchar(max)
+)
+go
+
+create table AspNetRoleClaims
+(
+    Id         int identity
+        constraint PK_AspNetRoleClaims
+            primary key,
+    RoleId     nvarchar(450) not null
+        constraint FK_AspNetRoleClaims_AspNetRoles_RoleId
+            references AspNetRoles
+            on delete cascade,
+    ClaimType  nvarchar(max),
+    ClaimValue nvarchar(max)
+)
+go
+
+create index IX_AspNetRoleClaims_RoleId
+    on AspNetRoleClaims (RoleId)
+go
+
+create unique index RoleNameIndex
+    on AspNetRoles (NormalizedName)
+    where [NormalizedName] IS NOT NULL
+go
+
+create table AspNetUsers
+(
+    Id                   nvarchar(450)              not null
+        constraint PK_AspNetUsers
+            primary key,
+    UserName             nvarchar(256),
+    NormalizedUserName   nvarchar(256),
+    Email                nvarchar(256),
+    NormalizedEmail      nvarchar(256),
+    EmailConfirmed       bit                        not null,
+    PasswordHash         nvarchar(max),
+    SecurityStamp        nvarchar(max),
+    ConcurrencyStamp     nvarchar(max),
+    PhoneNumber          nvarchar(max),
+    PhoneNumberConfirmed bit                        not null,
+    TwoFactorEnabled     bit                        not null,
+    LockoutEnd           datetimeoffset,
+    LockoutEnabled       bit                        not null,
+    AccessFailedCount    int                        not null,
+    Balance              decimal(18, 2) default 0.0 not null,
+    FirstName            nvarchar(max)  default N'' not null,
+    LastName             nvarchar(max)  default N'' not null
+)
+go
+
+create table AspNetUserClaims
+(
+    Id         int identity
+        constraint PK_AspNetUserClaims
+            primary key,
+    UserId     nvarchar(450) not null
+        constraint FK_AspNetUserClaims_AspNetUsers_UserId
+            references AspNetUsers
+            on delete cascade,
+    ClaimType  nvarchar(max),
+    ClaimValue nvarchar(max)
+)
+go
+
+create index IX_AspNetUserClaims_UserId
+    on AspNetUserClaims (UserId)
+go
+
+create table AspNetUserLogins
+(
+    LoginProvider       nvarchar(450) not null,
+    ProviderKey         nvarchar(450) not null,
+    ProviderDisplayName nvarchar(max),
+    UserId              nvarchar(450) not null
+        constraint FK_AspNetUserLogins_AspNetUsers_UserId
+            references AspNetUsers
+            on delete cascade,
+    constraint PK_AspNetUserLogins
+        primary key (LoginProvider, ProviderKey)
+)
+go
+
+create index IX_AspNetUserLogins_UserId
+    on AspNetUserLogins (UserId)
+go
+
+create table AspNetUserRoles
+(
+    UserId nvarchar(450) not null
+        constraint FK_AspNetUserRoles_AspNetUsers_UserId
+            references AspNetUsers
+            on delete cascade,
+    RoleId nvarchar(450) not null
+        constraint FK_AspNetUserRoles_AspNetRoles_RoleId
+            references AspNetRoles
+            on delete cascade,
+    constraint PK_AspNetUserRoles
+        primary key (UserId, RoleId)
+)
+go
+
+create index IX_AspNetUserRoles_RoleId
+    on AspNetUserRoles (RoleId)
+go
+
+create table AspNetUserTokens
+(
+    UserId        nvarchar(450) not null
+        constraint FK_AspNetUserTokens_AspNetUsers_UserId
+            references AspNetUsers
+            on delete cascade,
+    LoginProvider nvarchar(450) not null,
+    Name          nvarchar(450) not null,
+    Value         nvarchar(max),
+    constraint PK_AspNetUserTokens
+        primary key (UserId, LoginProvider, Name)
+)
+go
+
+create index EmailIndex
+    on AspNetUsers (NormalizedEmail)
+go
+
+create unique index UserNameIndex
+    on AspNetUsers (NormalizedUserName)
+    where [NormalizedUserName] IS NOT NULL
+go
+
+create table Transactions
+(
+    ID         int identity
+        constraint PK_Transactions
+            primary key,
+    Amount     decimal(18, 2) not null,
+    Date       datetime2      not null,
+    BankUserID nvarchar(450)
+        constraint FK_Transactions_AspNetUsers_BankUserID
+            references AspNetUsers,
+    Type       int default 0  not null
+)
+go
+
+create index IX_Transactions_BankUserID
+    on Transactions (BankUserID)
+go
+
+create table __EFMigrationsHistory
+(
+    MigrationId    nvarchar(150) not null
+        constraint PK___EFMigrationsHistory
+            primary key,
+    ProductVersion nvarchar(32)  not null
+)
+go
+
